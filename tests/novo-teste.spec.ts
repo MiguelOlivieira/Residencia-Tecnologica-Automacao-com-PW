2aabeb09164a22b87889c0dbcf5561be657050dd
Emmanuel Guerra <emmanuelguerra1945@gmail.com>
2025-05-23 11:21:30 -0300

    .

--- tests/novo-teste.spec.ts
+++ tests/novo-teste.spec.ts
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('fluxo completo do Swag Labs', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
