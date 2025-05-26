d031ed3a691fe7b48750ed8fd02a9c6751ddda3e
Emmanuel Guerra <emmanuelguerra1945@gmail.com>
2025-05-26 15:02:48 -0300

    Adicionei um teste que verifica a adição e remoção de produto da tela inicial no problem_user.

--- tests/swagLabs.spec.ts
+++ tests/swagLabs.spec.ts
@@ -54,32 +54,34 @@ await expect(itemMaisCaro).toBe('Sauce Labs Fleece Jacket');
 });
 
 
-//emmanuel
-test('fluxo completo do Swag Labs', async ({ page }) => {
+//Emmanuel
+test('GerenciamentoDoCarrinho', async ({ page }) => {
+
   await page.goto('https://www.saucedemo.com/');
 
+  //Login na conta 
+  await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('problem_user');
+  await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
 
-  await page.locator('[data-test="item-4-img-link"]').click();
-  await page.locator('[data-test="add-to-cart"]').click();
-  await page.locator('[data-test="back-to-products"]').click();
+  //Adicionar produto ao carrinho estando no menu principal
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  let produtosNoCarrinho = await page.locator('.shopping_cart_link').textContent();
+  //Recebe a quantidade de produtos no carrinho
 
-  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-  await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+  console.log("Você tem: "+produtosNoCarrinho+" produto(s) no carrinho.");
+  //Mostra no console a quantidade de produtos no carrinho
 
-  await page.locator('[data-test="shopping-cart-link"]').click();
-  await page.locator('[data-test="checkout"]').click();
+  //Remover o produto no menu principal
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+  await expect(produtosNoCarrinho).toBe(produtosNoCarrinho-1);
+  //Valida se a remoção de produto foi bem sucedida
 
-  await page.locator('[data-test="firstName"]').fill('abcde');
-  await page.locator('[data-test="lastName"]').fill('s');
-  await page.locator('[data-test="postalCode"]').fill('dasas');
-  await page.locator('[data-test="continue"]').click();
-
-  await page.locator('[data-test="error"]').click();
-  await page.locator('[data-test="checkout-info-container"] div').filter({ hasText: 'Error: Last Name is required' }).nth(2).click();
-
-  // Verificação simples: botão "Finish" visível
-  await expect(page.locator('[data-test="finish"]')).toBeVisible();
 });
+
+/*test('Page dos Produtos', async ({ page }) => {
+
+
+}*/
