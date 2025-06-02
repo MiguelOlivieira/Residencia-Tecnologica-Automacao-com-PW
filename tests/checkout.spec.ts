f22b761437cb7e64751453e6f9b1943be0b8b9f7
nicolasklayvert <nicolaskyny@gmail.com>
2025-06-02 14:35:55 -0300

    Correção nos três primeiros testes.

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -12,7 +12,7 @@ import { Logins } from '../users/logins';
 *   Após adicionar pelo menos um item no carrinho, deve ser possível concluir a compra 
 *   inserindo os dados do comprador (Primeiro nome, ultimo nome e CEP)
 */
-test('Checkout completo da compra', async ({ page }) => {
+test('Concluir Compra', async ({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
 
@@ -23,17 +23,13 @@ test('Checkout completo da compra', async ({ page }) => {
     await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
     await page.locator('[data-test="shopping-cart-link"]').click();
     await page.locator('[data-test="checkout"]').click();
-    await page.locator('[data-test="firstName"]').click();
     await page.locator('[data-test="firstName"]').fill('Nicolas');
-    await page.locator('[data-test="lastName"]').click();
     await page.locator('[data-test="lastName"]').fill('Nery');
-    await page.locator('[data-test="postalCode"]').click();
     await page.locator('[data-test="postalCode"]').fill('90265');
+    await page.locator('[data-test="continue"]').click();
+    await page.locator('[data-test="finish"]').click();
 
-    const validacao = 'https://www.saucedemo.com/checkout-step-one.html'
-
-
-    await expect(page).toHaveURL(validacao);
+    await expect(page).toHaveURL('https://www.saucedemo.com/checkout-complete.html');
 });
 
 /** 
@@ -52,18 +48,12 @@ test('Tela de Confirmação', async ({ page }) => {
     await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
     await page.locator('[data-test="shopping-cart-link"]').click();
     await page.locator('[data-test="checkout"]').click();
-    await page.locator('[data-test="firstName"]').click();
     await page.locator('[data-test="firstName"]').fill('Nicolas');
-    await page.locator('[data-test="lastName"]').click();
     await page.locator('[data-test="lastName"]').fill('Nery');
-    await page.locator('[data-test="postalCode"]').click();
     await page.locator('[data-test="postalCode"]').fill('90265');
     await page.locator('[data-test="continue"]').click();
 
-    const validacao = 'https://www.saucedemo.com/checkout-step-two.html'
-
-
-    await expect(page).toHaveURL(validacao);
+    await expect(page).toHaveURL('https://www.saucedemo.com/checkout-step-two.html')
 })
 
 /** 
@@ -80,19 +70,13 @@ test('Teste 3', async ({ page }) => {
     await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
     await page.locator('[data-test="shopping-cart-link"]').click();
     await page.locator('[data-test="checkout"]').click();
-    await page.locator('[data-test="firstName"]').click();
     await page.locator('[data-test="firstName"]').fill('Nicolas');
-    await page.locator('[data-test="lastName"]').click();
     await page.locator('[data-test="lastName"]').fill('Nery');
-    await page.locator('[data-test="postalCode"]').click();
     await page.locator('[data-test="postalCode"]').fill('90265');
     await page.locator('[data-test="continue"]').click();
     await page.locator('[data-test="cancel"]').click();
 
-    const validacao = 'https://www.saucedemo.com/inventory.html'
-
-
-    await expect(page).toHaveURL(validacao);
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
 })
 
 /**
