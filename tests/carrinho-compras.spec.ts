804f29f137ec74d53a880c9a6f555f683f58fce4
Emmanuel Guerra <emmanuelguerra1945@gmail.com>
2025-06-05 12:11:38 -0300

    Update carrinho-compras.spec.ts

--- tests/carrinho-compras.spec.ts
+++ tests/carrinho-compras.spec.ts
@@ -7,8 +7,9 @@
 import { test, expect } from '@playwright/test';
 import { Logins } from '../users/logins';
 
-//*Teste 01: Adiciona o primeiro
-test('GerenciamentoDoCarrinhoExpected', async ({ page }) => {
+//*Teste 01: Deve ser possível adicionar um produto ao carrinho através da tela inicial e o contador aumentar conforme a ocorrência
+//Emmanuel Guerra
+test('AdiçãoDeProdutoTelaInicial+ContadorAumentando', async ({ page }) => {
 
   await page.goto('https://www.saucedemo.com/');
 
@@ -23,8 +24,29 @@ test('GerenciamentoDoCarrinhoExpected', async ({ page }) => {
   let produtosNoCarrinho = parseInt(textoContador || '0');
   //Recebe a quantidade de produtos no carrinho
 
+  console.log("Você adicionou o produto com sucesso!");
+  
+
   console.log("Você tem: "+produtosNoCarrinho+" produto(s) no carrinho.");
-  //Mostra no console a quantidade de produtos no ho
+  //Mostra no console a quantidade de produtos no carrinho
+
+});
+
+//*Teste 02:Deve ser possível remover um produto do carrinho na tela inicial e o contador diminuir conforme a ocorrência
+//Emmanuel Guerra
+test('RemoçãoDeProdutoTelaInicial+ContadorReduzindo', async ({ page }) => {
+   await page.goto('https://www.saucedemo.com/');
+
+  //Login na conta 
+   const loginUser = new Logins(page);
+   await loginUser.login('standard_user', 'secret_sauce');
+   
+  //Adicionar produto ao carrinho estando no menu principal
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  let contador = await page.locator('.shopping_cart_badge');
+  let textoContador = await contador.textContent();
+  let produtosNoCarrinho = parseInt(textoContador || '0');
+  //Recebe a quantidade de produtos no carrinho
 
   //Remover o produto no menu principal
   await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
@@ -36,6 +58,64 @@ test('GerenciamentoDoCarrinhoExpected', async ({ page }) => {
   
 });
 
+//Emmanuel Guerra e Miguel Vieira
+//*Teste 03:Deve ser possível adicionar um produto ao carrinho na tela de produtos
+//Deve ser possível adicionar um produto ao carrinho estando na tela de visualização de produtos
+test('AdiçãoDeProdutoProductPage', async ({ page }) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+  //Login na conta 
+   const loginUser = new Logins(page);
+   await loginUser.login('standard_user', 'secret_sauce');
+   
+   await page.locator('[data-test="inventory-item-name"]', {hasText: 'Sauce Labs Backpack' }).click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=4');
+  //Espera a pagina do item selecionado ser carregada
+
+  await page.locator('#add-to-cart').click();
+  let contador = await page.locator('.shopping_cart_badge');
+  let textoContador = await contador.textContent();
+  let produtosNoCarrinho = parseInt(textoContador || '0');
+  //Recebe a quantidade de produtos no carrinho
+
+  console.log("O produto foi adicionado com sucesso!");
+
+
+});
+
+//*Teste 04:Deve ser possível remover um produto do carrinho na tela de produtos
+//Emmanuel Guerra
+test('RemoçãoDeProdutoProductPage', async ({ page }) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+  //Login na conta 
+   const loginUser = new Logins(page);
+   await loginUser.login('standard_user', 'secret_sauce');
+   
+   await page.locator('[data-test="inventory-item-name"]').click();
+  await page.goto('https://www.saucedemo.com/inventory-item.html?id=4')
+  //Espera a pagina do item selecionado ser carregada
+
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  let contador = await page.locator('.shopping_cart_badge');
+  let textoContador = await contador.textContent();
+  let produtosNoCarrinho = parseInt(textoContador || '0');
+  //Recebe a quantidade de produtos no carrinho
+
+  //Remover o produto na Product Page
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+
+  await expect(page.locator('.shopping_cart_badge')).toHaveCount(0);
+  //Valida se a remoção de produto foi bem sucedida
+
+
+  console.log("O produto foi removido com sucesso!");
+
+});
+
+//*Teste 05:No carrinho de compras deve ser apresentado todos os produtos adicionados contendo nome, descrição detalhada e preço do produto
 //Emmanuel Guerra e Miguel Vieira
 test('Exibição de produtos selecionados no carrinho', async ({page}) => {
   await page.goto('https://www.saucedemo.com/');
@@ -120,7 +200,8 @@ test('Exibição de produtos selecionados no carrinho', async ({page}) => {
 
 });
 
-//Emmanuel Guerra e Miguel Vieira
+//*Teste 06:No carrinho de compras deve ser possível remover o produto
+//Miguel Vieira
 test('Remoção de item diretamente do carrinho', async ({page}) => {
   await page.goto('https://www.saucedemo.com/');
 
@@ -143,13 +224,8 @@ test('Remoção de item diretamente do carrinho', async ({page}) => {
 
 });
 
-
-//TO DO 
-test('Alterar quantidade de produtos e limitar a 99', async ({page}) => {
-
-});
-
-//Emmanuel Guerra e Miguel Vieira
+//*Teste 07:É possível seguir com a compra através do botão de checkout
+//Miguel Vieira
 test('Seguir com compra após clicar no botão Checkout', async ({page}) => {
  await page.goto('https://www.saucedemo.com/');
 
@@ -168,21 +244,41 @@ test('Seguir com compra após clicar no botão Checkout', async ({page}) => {
      //Espera-se que a URL Após clicar em checkout, seja a URL correta.
     await expect(page).toHaveURL(enderecoEsperado);
 
-    console.log("Checkout acessado com sucesso!")
-
-
+    console.log("Checkout acessado com sucesso!");
 
 });
 
 
 
-// TO DO
+//*Teste 08:Deve possível voltar para tela de produtos através do botão continue shopping
+//Emmanuel Guerra
 test('Voltar a tela de produtos após clicar em "continue shopping" ', async ({page}) => {
 
+  await page.goto('https://www.saucedemo.com/');
+
+   const loginUser = new Logins(page);
+   await loginUser.login('standard_user', 'secret_sauce');
+
+   const enderecoEsperado = "https://www.saucedemo.com/inventory.html";
+
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+    //Ir até ao carrinho
+    await page.locator('[data-test="shopping-cart-link"]').click();
+    
+    //Clicar em continuar comprando 
+    await page.locator('#continue-shopping').click();
+    
+    //Valida se o URL acessado no continue shopping é o correto
+    await expect(page).toHave(enderecoEsperado);
+
+  console.log("Você voltou a pagina inicial!");
   
 });
 
-test('GerenciamentoDoCarrinhoError', async ({ page }) => {
+//Teste Error 01:
+//Emmanuel Guerra
+test('RemoçãoProdutoTelaInicialError', async ({ page }) => {
 
   await page.goto('https://www.saucedemo.com/');
 
